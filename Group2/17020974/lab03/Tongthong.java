c84e23226534e31efe79a06a58e045231895241b
cxqntnt <cxqcxq12345@gmail.com>
2018-09-25 17:47:29 +0700

    Quang

---
+++ Group2/17020974/lab03/Tongthong.java
@@ -0,0 +1,13 @@
+public class Tongthong{
+	private String country;
+	private String gioitinh;
+	private int age;
+	public void setCountry(String a){this.country=a;}
+	public String getCountry(){return country;}
+
+	public void setGioitinh(String b){this.gioitinh=b;}
+	public String getGioitinh(){return gioitinh;}
+
+	public void setAge(int c){this.age=c;}
+	public int getAge(){return age;}
+}
