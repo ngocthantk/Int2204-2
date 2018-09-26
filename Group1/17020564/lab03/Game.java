1a9f3575bfd8efec50b474142bdbe8fd93a6fe6c
vuanhlk12 <vuanhlk12@gmail.com>
2018-09-26 23:35:39 +0700

    nop bai lab 3

---
+++ Group1/17020564/lab03/Game.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tuan3;
+
+/**
+ *
+ * @author FEELSBADMAN
+ */
+public class Game {
+    private String theloai;
+    private boolean multiplayer;
+
+    public String getTheloai() {
+        return theloai;
+    }
+
+    public void setTheloai(String theloai) {
+        this.theloai = theloai;
+    }
+
+    public boolean isMultiplayer() {
+        return multiplayer;
+    }
+
+    public void setMultiplayer(boolean multiplayer) {
+        this.multiplayer = multiplayer;
+    }
+    
+}
