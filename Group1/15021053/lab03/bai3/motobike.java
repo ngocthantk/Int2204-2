ae014a0c513c04d141e8bb56f98204f6e454e5d4
daoduongvt97 <daoduongvt971@gmail.com>
2018-09-26 20:50:48 +0700

    lab03

---
+++ Group1/15021053/lab03/bai3/motobike.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab03;
+
+/**
+ *
+ * @author CCNE
+ */
+public class motobike {
+    public String congSuat;
+    public String hangSanXuat;
+    public String ten;
+
+    public String getCongSuat() {
+        return congSuat;
+    }
+
+    public void setCongSuat(String congSuat) {
+        this.congSuat = congSuat;
+    }
+
+    public String getHangSanXuat() {
+        return hangSanXuat;
+    }
+
+    public void setHangSanXuat(String hangSanXuat) {
+        this.hangSanXuat = hangSanXuat;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+    
+    public void tangToc(){}
+    public void dungLai(){}
+}
