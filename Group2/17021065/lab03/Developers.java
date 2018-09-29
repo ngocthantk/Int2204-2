9ce330e95f49b2475d71361fe8e84008b18cd311
Dang Tran Toan <sieogiti7272@gmail.com>
2018-09-29 18:16:07 +0700

    Toan commit

---
+++ Group2/17021065/lab03/Developers.java
@@ -0,0 +1,39 @@
+package tuan2;
+
+public class Developers {
+	private String LanDeve; // viet dc ngon ngu nao
+	private int Exp; // kinh nghiem (year)
+	private String Achieve; // thanh tuu
+	private String name;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLanDeve() {
+		return LanDeve;
+	}
+
+	public void setLanDeve(String lanDeve) {
+		LanDeve = lanDeve;
+	}
+
+	public int getExp() {
+		return Exp;
+	}
+
+	public void setExp(int exp) {
+		Exp = exp;
+	}
+
+	public String getAchieve() {
+		return Achieve;
+	}
+
+	public void setAchieve(String achieve) {
+		Achieve = achieve;
+	}
+}
