587f7a8fd15ee195aa9f4e1277fc12efd33fc50f
Tranmai041099 <43170956+Tranmai041099@users.noreply.github.com>
2018-10-09 23:05:06 +0700

    Mai submit lab05

---
+++ Group2/17020876/lab05/Rectangle.java
@@ -0,0 +1,50 @@
+package Shape;
+
+public class Rectangle extends Shape{
+	protected double width = 1.0;
+	protected double length = 1.0;
+	
+	public Rectangle(){}
+	public Rectangle( double width, double length)
+	{
+            this.width = width;
+            this.length = length;
+	}
+	
+	public Rectangle( double width, double length, String color, Boolean filled)
+	{
+            super(color, filled);
+            this.width = width;
+            this.length = length;
+            	
+	}
+	public double getWidth()
+        {
+            return width;
+        }
+	public void setWidth(double width)
+        {
+            this.width = width;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+	public void setLength(double length)
+        {
+            this.length = length;
+        }
+        public double getArea()
+        {
+            return this.width*this.length;
+        }
+	public double getPerimeter()
+        {
+            return (this.width+this.length)*2;
+        }
+	@Override
+	public String toString()
+        {
+            return this.color + "," + this.filled + "," + this.width + "," + this.length;
+        }
+}
