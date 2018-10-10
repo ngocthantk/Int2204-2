5fb15e6df58d2d96721cd11e581e96de479d3ce9
dong <dongpham120899@gmail.com>
2018-10-10 08:57:10 +0700

    lab5

---
+++ Group1/17020687/lab05/Lab5/Battap1/src/QuaTao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CCNE
+ */
+public class QuaTao extends HoaQua{
+	String loaiTao;
+
+    public String getLoaiTao() {
+        return loaiTao;
+    }
+
+    public void setLoaiTao(String loaiTao) {
+        this.loaiTao = loaiTao;
+    }
+    
+}
