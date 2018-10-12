1884e142f93b514c0e0b79926ff743f130c043b1
BlancTuan <43158523+BlancTuan@users.noreply.github.com>
2018-10-12 14:55:25 +0700

    Add files via upload

---
+++ Group2/17021109/lab05/Shape.java
@@ -0,0 +1,35 @@
+package Example2;
+
+public class Shape {
+	protected String color = "red";
+	protected Boolean filled = true;
+
+	public Shape() {
+	}
+
+	public Shape(String color, Boolean filled) {
+		this.color = color;
+		this.filled = filled;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Boolean getFilled() {
+		return filled;
+	}
+
+	public void setFilled(Boolean filled) {
+		this.filled = filled;
+	}
+
+	@Override
+	public String toString() {
+		return this.color + "," + this.filled;
+	}
+}
