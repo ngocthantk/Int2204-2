709c345490da4bc72db85103eaa29a1b26de39c8
nguyenminhnghia <43170755+nguyenminhnghia@users.noreply.github.com>
2018-10-13 15:05:36 +0700

    Nguyễn Minh Nghĩa-17020925

---
+++ Group2/17020925/lab05/CamSanh.java
@@ -0,0 +1,14 @@
+package Fruit;
+
+public class CamSanh extends QuaCam{
+    
+	private double can;
+
+	public double getCan() {
+		return can;
+	}
+	public void setCan(double can) {
+		this.can = can;
+	}
+	
+}
