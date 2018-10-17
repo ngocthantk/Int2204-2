9bdff251887895c2ebeaf34661513bec5b228c3e
nguyenminhnghia <43170755+nguyenminhnghia@users.noreply.github.com>
2018-10-17 22:09:05 +0700

    Nguyễn Minh Nghĩa

---
+++ Group2/17020925/lab05/QuaCam.java
@@ -0,0 +1,13 @@
+package Fruit;
+
+public class QuaCam extends Fruit{
+	protected String nguongoc ;
+	public String getNguongoc()
+	{
+	return nguongoc;
+	}
+	public void setNguongoc(String nguongoc)
+	{
+	this.nguongoc = nguongoc;
+	}
+}
