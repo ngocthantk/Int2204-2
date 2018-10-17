ce13a08d627d2538665b94e5018336ca5a1cea15
minhnuos <43170763+minhnuos@users.noreply.github.com>
2018-10-17 15:54:58 +0700

    Minh

---
+++ Group2/17020900/lab06/Circle.java
@@ -0,0 +1,20 @@
+package minh.dim;
+
+public class Circle extends Shape {
+	protected toado O;
+	public Circle(toado o, double bankinh) {
+		
+		O = o;
+		this.bankinh = bankinh;
+	}
+	final double PI = 3.14;
+	protected double bankinh;
+	public double chuvi() {
+		
+		return  2*PI*bankinh;
+	}
+	public double dientich() {
+		
+		return PI*bankinh*bankinh;
+	}
+}
