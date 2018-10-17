271722b9f1179fb89f06111937fc6da17409ad6c
namhaivu99 <43141316+namhaivu99@users.noreply.github.com>
2018-10-17 09:46:15 +0700

    Update Main.java

--- Group2/17020919/lab06/Main.java
+++ Group2/17020919/lab06/Main.java
@@ -40,17 +40,17 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Layer la1=new Layer();
-		Square s1=new Square(3);
+		Square s1=new Square(3,3,3,5,5,3,5,5);
 		la1.layer.add(s1);		
-		Circle c1=new Circle(2);
+		Circle c1=new Circle(2,2,2);
 		la1.layer.add(c1);
-		Triangle t1=new Triangle(1,2,3);
+		Triangle t1=new Triangle(1,2,3,2,5,6);
 		la1.layer.add(t1);
-		Circle c11=new Circle(3);
+		Circle c11=new Circle(3,1,1);
 		la1.layer.add(c11);
-		Rectangle r1=new Rectangle(2,3);
+		Rectangle r1=new Rectangle(2,3,2,5,5,2,5,5);
 		la1.layer.add(r1);
-		Circle c12=new Circle(4);
+		Circle c12=new Circle(4,2,5);
 		la1.layer.add(c12);		
 		Diagram d=new Diagram();	
 		d.dia.add(la1);
