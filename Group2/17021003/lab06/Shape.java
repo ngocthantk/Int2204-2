bc20c8e7d049b1a1aa0928d4a4fbee3e0a936725
CongSon99 <43170886+overin999@users.noreply.github.com>
2018-10-18 21:51:56 +0700

    Add files via upload

---
+++ Group2/17021003/lab06/Shape.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6;
+
+/**
+ *
+ * @author CCNE
+ */
+public class Shape {
+
+    public String name;
+
+    public String color = "red";
+
+    public Shape() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return this.color + " " + this.name;
+
+    }
+
+}
