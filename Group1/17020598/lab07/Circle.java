8ff1ed102bb7f6df36057b695c97c192fca09e3e
anhtam11061999 <43164984+anhtam11061999@users.noreply.github.com>
2018-10-24 23:51:37 +0700

    LAB7

---
+++ Group1/17020598/lab07/Circle.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7;
+
+/**
+ *
+ * @author AnhTam
+ */
+public class Circle extends Shape{
+    protected String color;
+    protected int bankinh;
+    
+    Circle(){
+        
+    }
+    Circle(int x,int y,int r){
+        this.bankinh=r;
+        this.x=x;
+        this.y=y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getBankinh() {
+        return bankinh;
+    }
+
+    public void setBankinh(int bankinh) {
+        this.bankinh = bankinh;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    
+}
